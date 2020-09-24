fde3f42a56f4e06681ef37429f0144f0e63ead4f
Gyeltshen71 <12190049.gcit@rub.edu.bt>
2020-09-24 21:50:25 +0600

    Initial Commit

---
+++ Lab_works/Lab_00/arrayDemo.java
@@ -0,0 +1,8 @@
+public class arrayDemo {
+	public static void main(String[] args) {
+		int array[] = {1,2,3};
+		for(int i = 0; i < array.length; i++) {
+			System.out.println("The array members are " + array[i]);
+		}
+	}
+}
